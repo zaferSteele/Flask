90f1032a48655abbee0ed37038f69b43ec0e8d70
Zafer Steele <zafersteele@gmail.com>
2025-06-22 11:52:06 +0100

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_networkers():
-    return 'Hello Networkers!'
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
