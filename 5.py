5b50d2d0b3b1375bed96a5c7fccbd95f70d7b5cb
Zafer Steele <zafersteele@gmail.com>
2025-06-22 11:52:37 +0100

    Delete 5.py

--- 5.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/routers/<hostname>/interface/<int:interface_number>')
-def interface(hostname, interface_number):
-    return jsonify(name=hostname, interface=interface_number)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
